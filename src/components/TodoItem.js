c87a53bb67f8eb16f9f851d97e4921739bec75a0
Samantha <samantha@Samanthas-MacBook-Air.local>
2022-02-14 10:40:53 -0600

    delete func for todo list

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,8 +5,14 @@ class TodoItem extends React.Component {
     return <li>
     <input type="checkbox"
           checked={this.props.todo.completed}
-          onChange={() => this.props.handleChangeProps(this.props.todo.id)}/>
+          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+          />
     {this.props.todo.title}
+
+    <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+      Delete
+    </button>
+
     </li>
   }
 }
