5a6d80e7e59a7ae0c218873a43f41bcc9148b5f6
Samantha <samantha@Samanthas-MacBook-Air.local>
2022-02-13 21:15:18 -0600

    child component lists through props

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,4 +1,7 @@
 import React from "react"
+import TodosList from "./TodosList";
+
+
 class TodoContainer extends React.Component {
 
   state = {
@@ -22,17 +25,12 @@ class TodoContainer extends React.Component {
 };
 
 
-      render() {
-return (
-  <ul>
-    {
-      this.state.todos.map(
-        todo => (
-              <li>{todo.title}</li>
-            ))
-  }
-  </ul>
-);
+render() {
+  return (
+    <div>
+      <TodosList todos={this.state.todos} />
+    </div>
+  );
 }
 
 }
